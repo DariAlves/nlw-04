38528004d530044c75d48c806e2b06e347c945be
Dari Alves <darieltondf@gmail.com>
2021-02-23 10:33:52 -0300

    migrando projeto para nextjs

---
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '50%' }} />
+
+                <span 
+                    className="current-experience"
+                    style={{ left: '50%' }}
+                >
+                    300 px
+                </span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    )
+}
