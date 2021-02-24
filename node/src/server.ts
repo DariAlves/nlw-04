da8fdf8a0ea2a45dbc0b2c588e7cfee7c73067f8
Dari Alves <darieltondf@gmail.com>
2021-02-23 22:31:07 -0300

    migrations, typeorm, controllers, routes, database

--- node/src/server.ts
+++ node/src/server.ts
@@ -1,14 +1,12 @@
+import 'reflect-metadata';
+import './database';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello node'})
-});
-
-app.post('/', (request, response) => {
-    return response.json({ message: 'Dados salvos com sucesso!'})
-});
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => {
     console.log("Server is running!")
