8f3b55191d3cf87c80c80b73b188fc9157cc4c9a
Dari Alves <darieltondf@gmail.com>
2021-02-26 19:37:32 -0300

    refatorar controller, repository de usuario

--- node/src/controllers/UserController.ts
+++ node/src/controllers/UserController.ts
@@ -1,13 +1,25 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        // const usersRepository = getRepository(User); Antes de separar as funcionalidades do repositório
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        /* SELECT * FROM USERS WHERE EMAIL = "EMAIL" */
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: "User already exists!"
+            })
+        }
 
         const user = usersRepository.create({
             name,
@@ -21,4 +33,4 @@ class UserController {
 
 }
 
-export { UserController }
+export { UserController };
